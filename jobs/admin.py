8175f50de90463d7b95b68bb61d3e91206032ef7
flashdrag <flashdrag@gmail.com>
2023-06-02 22:06:28 +0100

    Create `Vacancy` db model #3

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Vacancy
+
+
+@admin.register(Vacancy)
+class VacancyAdmin(admin.ModelAdmin):
+    '''Define admin model for custom Vacancy model.'''
+
+    # The fields which displayed in the list of vacancies in admin panel
+    list_display = (
+        "status",
+        "title",
+        # "employer",
+        # "company",
+        "area",
+        "job_location",
+        "job_type",
+        "salary",
+        "created_on",
+        "experience",
+    )
+    # list of fields to generate filters in the right sidebar of admin panel
+    list_filter = (
+        "status",
+        "area",
+        # "employer",
+        # "company",
+        "job_type",
+        "job_location",
+        "salary",
+    )
+    search_fields = ("title", "body")
+
+    # Sorted by vacancies that are in review and with the oldest created date
+    ordering = ("status", "created_on")
--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,97 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+IRELAND_COUNTIES = (
+    ("DUBLIN", "Dublin"),
+    ("DUBLIN_CITY_CENTRE", "Dublin City Centre"),
+    ("DUBLIN_NORTH", "Dublin North"),
+    ("DUBLIN_SOUTH", "Dublin South"),
+    ("DUBLIN_WEST", "Dublin West"),
+    ("CARLOW", "Carlow"),
+    ("CAVAN", "Cavan"),
+    ("CLARE", "Clare"),
+    ("CORK", "Cork"),
+    ("DONEGAL", "Donegal"),
+    ("GALWAY", "Galway"),
+    ("KERRY", "Kerry"),
+    ("KILDARE", "Kildare"),
+    ("KILKENNY", "Kilkenny"),
+    ("LAOIS", "Laois"),
+    ("LEITRIM", "Leitrim"),
+    ("LIMERICK", "Limerick"),
+    ("LONGFORD", "Longford"),
+    ("LOUTH", "Louth"),
+    ("MAYO", "Mayo"),
+    ("MEATH", "Meath"),
+    ("MONAGHAN", "Monaghan"),
+    ("OFFALY", "Offaly"),
+    ("ROSCOMMON", "Roscommon"),
+    ("SLIGO", "Sligo"),
+    ("TIPPERARY", "Tipperary"),
+    ("WATERFORD", "Waterford"),
+    ("WESTMEATH", "Westmeath"),
+    ("WEXFORD", "Wexford"),
+    ("WICKLOW", "Wicklow"),
+    ("NORTHERN_IRELAND", "Northern Ireland"),
+    ("UK", "UK"),
+    ("EUROPE", "Europe"),
+    ("WORLDWIDE", "Worldwide"),
+)
+
+JOB_TYPES = (
+    ("FULL_TIME", "Full Time"),
+    ("PART_TIME", "Part Time"),
+    ("CONTRACT", "Contract"),
+    ("PERMANENT", "Permanent"),
+    ("TEMPORARY", "Temporary"),
+    ("INTERNSHIP", "Internship"),
+    ("VOLUNTEER", "Volunteer"),
+)
+
+JOB_LOCATIONS = (
+    ("ON_SITE", "On Site"),
+    ("REMOTE", "Remote"),
+    ("HYBRID", "Hybrid"),
+)
+
+JOB_POST_STATUS = (
+    (0, "IN_REVIEW"),
+    (1, "ACTIVE"),
+    (2, "CLOSED"),
+)
+
+
+class Vacancy(models.Model):
+    title = models.CharField(max_length=255, blank=False)
+    """
+    # TODO: add FKs to Employer and Company models
+    employer = models.ForeignKey(
+        Employer, on_delete=models.CASCADE, related_name="vaccancies"
+    )
+    company = models.ForeignKey(
+        Company, on_delete=models.CASCADE, related_name="vaccancies"
+    )
+    """
+    body = models.TextField(blank=False)
+    area = models.CharField(
+        choices=IRELAND_COUNTIES, max_length=50, blank=False
+    )
+    job_location = models.CharField(
+        max_length=50, choices=JOB_LOCATIONS, blank=False
+    )
+    job_type = models.CharField(choices=JOB_TYPES, max_length=50, blank=False)
+    salary = models.CharField(max_length=50, default="Negotiable", blank=False)
+    experience = models.CharField(max_length=50, blank=True, null=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=JOB_POST_STATUS, default=0)
+
+    class Meta:
+        # the vaccancies will be ordered by the date they were created,
+        # with the most recent ones at the top
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("jobs:vacancy_detail", args=[str(self.id)])
