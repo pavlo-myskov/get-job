15afcbc3120731df95cb938783fa3455c5fbc1e5
flashdrag <flashdrag@gmail.com>
2023-06-04 22:46:52 +0100

    Add dynamic form placeholder

--- jobseeker/views.py
+++ jobseeker/views.py
@@ -6,15 +6,18 @@ from jobs.forms import SearchForm
 
 class HomeView(ListView):
     # object name that will be used in the template
-    context_object_name = 'job_list'
+    context_object_name = "job_list"
     # get only first 4 active vacancies
     queryset = Vacancy.objects.filter(status=Vacancy.JobPostStatus.ACTIVE)[:4]
-    template_name = 'jobseeker/home.html'
+    template_name = "jobseeker/home.html"
 
     def get_context_data(self, **kwargs):
-        '''Add search form to the context'''
+        """Add search form to the context"""
         # the method allows to avoid overriding get() method
         # and adding the form to the context there
         context = super(HomeView, self).get_context_data(**kwargs)
-        context['form'] = SearchForm()
+        context["main_form"] = SearchForm(
+            placeholder="e.g. full stack software developer"
+        )
+        context["navbar_form"] = SearchForm(placeholder="search job")
         return context
