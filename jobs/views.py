0000ccc4d4422b89be2073e6d46e8e5179b5f3f9
flashdrag <flashdrag@gmail.com>
2023-06-06 19:24:24 +0100

    Fix job search results view
    - Return only approved jobs
    - Show all aprroved jobs by `/jobs` url

--- jobs/views.py
+++ jobs/views.py
@@ -8,16 +8,23 @@ class JobListView(ListView):
     model = Vacancy
 
     def get_queryset(self):
+        '''
+        Search for aproved jobs by title if search query is provided,
+        otherwise return all approved jobs (even if empty query is provided)
+        '''
         # get the query parameter from the URL
         # e.g. /jobs?search=developer, then query = developer
-        query = self.request.GET.get("search").strip()
-        # TODO add test empty string
-        # filter titles that contain the search query
+        query = self.request.GET.get("search")
         # icontains allows lookup is case insensitive
-        if query:
-            job_list = Vacancy.objects.filter(title__icontains=query)
+        if query and query.strip():
+            job_list = Vacancy.objects.filter(
+                title__icontains=query,
+                status=Vacancy.JobPostStatus.ACTIVE,
+            )
             # TODO: add search by area using Q objects
             # (Q(title__icontains=query) | Q(area__icontains=query))
         else:
-            job_list = Vacancy.objects.all()
+            job_list = Vacancy.objects.filter(
+                status=Vacancy.JobPostStatus.ACTIVE,
+            )
         return job_list
