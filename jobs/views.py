9837d715f3a4bb14c50ae5933370c2d18ec8952e
flashdrag <flashdrag@gmail.com>
2023-06-07 21:04:47 +0100

    Add search panel for vacancy list page #53 #57

--- jobs/views.py
+++ jobs/views.py
@@ -32,5 +32,8 @@ class JobListView(ListView):
 
     def get_context_data(self, **kwargs):
         """Add search form to the context"""
+        kwargs["main_form"] = SearchForm(
+            placeholder="e.g. full stack software developer"
+        )
         kwargs["navbar_form"] = SearchForm(placeholder="search job")
         return super().get_context_data(**kwargs)
