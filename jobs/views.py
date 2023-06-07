5879d9d9683b4f7056f32fb822b507f7823efda9
flashdrag <flashdrag@gmail.com>
2023-06-07 19:29:26 +0100

    Add navbar with search bar to the vacancy list page

--- jobs/views.py
+++ jobs/views.py
@@ -29,3 +29,8 @@ class JobListView(ListView):
             )
 
         return job_list
+
+    def get_context_data(self, **kwargs):
+        """Add search form to the context"""
+        kwargs["navbar_form"] = SearchForm(placeholder="search job")
+        return super().get_context_data(**kwargs)
