8e71fc25c3e5cfcdd02bdec4774e3aef66ff2615
flashdrag <flashdrag@gmail.com>
2023-06-08 14:56:12 +0100

    Refactor SearchForm: set fields from Vacancy model #50

--- jobs/forms.py
+++ jobs/forms.py
@@ -1,17 +1,47 @@
 from django import forms
 
+from .models import Vacancy
 
-class SearchForm(forms.Form):
-    title = forms.CharField(
-        label="Job Title",
-        required=False,
-        max_length=255,
-        widget=forms.TextInput(
-            attrs={
+
+class SearchForm(forms.ModelForm):
+    class Meta:
+        model = Vacancy
+        fields = ["title", "area", "job_location", "job_type"]
+
+    def __init__(self, *args, **kwargs):
+        """Override init method to set custom settings for fields"""
+        super().__init__(*args, **kwargs)
+
+        # set fields as not required
+        for field in self.fields.values():
+            field.required = False
+
+        self.fields["title"].label = "Job Title"
+
+        # set custom widget attributes
+        self.fields["title"].widget.attrs.update(
+            {
                 "class": "form-control search-bar purple-input",
                 "type": "search",
                 "aria-label": "Search job",
                 "placeholder": "search job",
             }
-        ),
-    )
+        )
+
+        self.fields["area"].widget.attrs.update(
+            {
+                "class": "purple-input",
+            }
+        )
+
+        self.fields["job_location"].widget.attrs.update(
+            {
+                "class": "purple-input",
+            }
+        )
+
+        self.fields["job_type"].widget.attrs.update(
+            {
+                "class": "purple-input",
+            }
+        )
