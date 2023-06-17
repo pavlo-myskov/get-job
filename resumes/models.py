992c8d7e366da9aef2c9a4d4607c3743b5d048dd
flashdrag <flashdrag@gmail.com>
2023-06-17 13:51:48 +0100

    Add status and auto date for resume model

--- resumes/models.py
+++ resumes/models.py
@@ -7,6 +7,13 @@ from jobseeker.models import Jobseeker
 
 
 class Resume(models.Model):
+
+    class ResumePublishStatus(models.TextChoices):
+        IN_REVIEW = "IN_REVIEW", "In Review"
+        ACTIVE = "ACTIVE", "Active"
+        WITHDRAWN = "WITHDRAWN", "Withdrawn"
+        CLOSED = "CLOSED", "Closed"
+
     jobseeker = models.ForeignKey(
         Jobseeker, on_delete=models.CASCADE, related_name="resumes"
     )
@@ -18,6 +25,21 @@ class Resume(models.Model):
     cv = models.FileField(
         upload_to="cv/", blank=True, storage=RawMediaCloudinaryStorage()
     )
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(
+        choices=ResumePublishStatus.choices,
+        max_length=50,
+        default=ResumePublishStatus.IN_REVIEW,
+    )
+
+    class Meta:
+        # ordered by the date they were created,
+        # with the most recent ones at the top
+        ordering = ["-created_on"]
 
     def __str__(self) -> str:
         return f'{self.occupation} - {self.jobseeker}'
+
+    # TODO:
+    # def get_absolute_url(self):
+    #     return reverse("resumes:resume_detail", args=[str(self.id)])
