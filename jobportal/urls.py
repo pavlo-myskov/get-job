64d07bf594d3498d501a4ef1d2869d0d89118e46
flashdrag <flashdrag@gmail.com>
2023-06-28 18:02:44 +0100

    Add custom 500 internal server http error

--- jobportal/urls.py
+++ jobportal/urls.py
@@ -6,11 +6,13 @@ from users.views import (
     custom_bad_request_view,
     custom_permission_denied_view,
     custom_page_not_found_view,
+    custom_server_error_view,
 )
 
 handler400 = custom_bad_request_view
 handler403 = custom_permission_denied_view
 handler404 = custom_page_not_found_view
+handler500 = custom_server_error_view
 
 urlpatterns = [
     path("admin/", admin.site.urls),
