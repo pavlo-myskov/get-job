a4c9542df1832723eaead4d6d15cfa13073421ec
flashdrag <flashdrag@gmail.com>
2023-07-05 20:19:43 +0100

    Add EmployerRequiredMixin

--- employer/views.py
+++ employer/views.py
@@ -1,7 +1,36 @@
 from django.views.generic import ListView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.shortcuts import render
+from django.contrib import messages
+
+from users.models import User
 
 from resumes.models import Resume
 from resumes.forms import ResumeSearchForm
+
+
+class EmployerRequiredMixin(LoginRequiredMixin, UserPassesTestMixin):
+    def test_func(self):
+        """Check if the user is a Employer"""
+        return self.request.user.role == User.Role.EMPLOYER
+
+    def handle_no_permission(self):
+        """
+        Redirect to the specific page based on the user's authentication
+        status and role:
+        - Redirect to the login page if the user is not authenticated.
+        - Display the 403 employer page if the role is not EMPLOYER.
+        - Otherwise, display the default 403 page.
+        """
+        if not self.request.user.is_authenticated:
+            message = "Sign in or create an account"
+            " as an Employer to access this page"
+            messages.add_message(self.request, messages.WARNING, message)
+            return super().handle_no_permission()
+        elif self.request.user.role != User.Role.EMPLOYER:
+            return render(self.request, "errors/employer_403.html", status=403)
+        else:
+            return super().handle_no_permission()
 
 
 class HomeView(ListView):
