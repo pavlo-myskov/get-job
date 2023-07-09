f6e3f52a9dca3f1f2913c66017f9a74c2e584bf8
flashdrag <flashdrag@gmail.com>
2023-07-09 11:34:00 +0100

    Add Employer Profile to admin panel

--- employer/admin.py
+++ employer/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import EmployerProfile
+
+
+@admin.register(EmployerProfile)
+class EmployerProfileAdmin(admin.ModelAdmin):
+    '''Define admin model for EmployerProfile model.'''
+
+    list_display = ['user', 'name', 'company']
+    search_fields = ['user__email', 'name', 'company']
